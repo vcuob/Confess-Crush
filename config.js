39dff79042fe39445c0f65219511ffdbb0a09e6a
vcuob <94881685+vcuob@users.noreply.github.com>
2021-11-23 11:58:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,22 @@
 const CONFIG = {
     titleWeb: "Gửi đến Dương",
     introTitle: 'Nguyễn Thị Thùy Dương',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    introDesc: `Cùng anh băng qua bao đại dương
+Cùng anh đi vượt ngàn con đường
+Phiêu lãng như áng mây trời
+Xanh ngát như giấc mơ ta
+Và đời vẫn thế, vẫn mãi trôi....
+bài hát e thích đúng ko nè`,
+    btnIntro: 'Ấn vào đây để tiếp tục nè',
+    title: 'Em thích ai?',
+    desc: 'Chọn 1 trong 2 đi nè :3 ',
+    btnYes: 'Vũ Hoàng Anh',
+    btnNo: '1 người khác',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao e lại yêu a ? <3',
+    btnReply: 'Gửi cho Hoàng Anh',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘 Tha thứ cho a nha',
-    messDesc: 'iuuu.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Anh biết mà . Yêu em nhiều nhiều 😘😘 Tha thứ cho a nha',
+    messDesc: 'a thấy im lặng mãi cx ko giải quyết được gì nên mới làm cái này nè',
+    btnAccept: 'Đồng ý tha thứ <3',
     messLink: 'https://www.facebook.com/messages/t/100035570839753' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }

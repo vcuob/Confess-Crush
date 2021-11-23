d557ef03258e7fa70623a1ebae59ed8a79b84d92
vcuob <94881685+vcuob@users.noreply.github.com>
2021-11-23 09:56:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi đến Dương",
+    introTitle: 'Nguyễn Thị Thùy Dương',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
